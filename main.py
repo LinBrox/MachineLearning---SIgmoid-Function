04cf8968adb743a2ab5a6bf0c3b600cd80199739
LinBrox <97131958+LinBrox@users.noreply.github.com>
2023-02-19 12:20:37 +0000

    Made some serious changes - converted arff to csv is now more streamlined I have also implemented a simple 3 layer neural network steps 1-5 complete

--- main.py
+++ main.py
@@ -1,75 +1,56 @@
-import numpy as np
 import pandas as pd
-import matplotlib.pyplot as plt
-import matplotlib
-matplotlib.use('TkAgg')
-from sklearn.impute import KNNImputer
-from sklearn.preprocessing import StandardScaler
+import numpy as np
+import tensorflow as tf
 from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import MinMaxScaler
+import matplotlib.pyplot as plt
 
-# Task 1 - part 1
-# Loads the ARFF and gives it the variable name to 'df' into the Pandas Dataframe
-df = pd.read_csv('messidor_features.arff', header=None, comment='@')
-
-# These are the headers used for the CSV file
 headerList = ['Quality Assessment', 'Pre-Screening', 'MA Detection 1', 'MA Detection 2', 'MA Detection 3',
               'MA Detection 4', 'MA Detection 5', 'MA Detection 6', 'Exudates Detection 1',
               'Exudates Detection 2', 'Exudates Detection 3', 'Exudates Detection 4',
               'Exudates Detection 5', 'Exudates Detection 6', 'Exudates Detection 7',
               'Euclidean Distance', 'OPTIC Disc', 'AM/FM', 'Output', 'Output2(Can be Dropped)']
 
-# Replace missing values with NaN
-df.replace('?', np.nan, inplace=True)
+# 1. Convert ARFF to CSV
+with open('messidor_features.arff') as f:
+    content = f.readlines()
+content = [x.strip() for x in content]
+start_data = content.index('@data') + 1
+data = [x.split(',') for x in content[start_data:]]
+df = pd.DataFrame(data, columns=headerList)
+df.to_csv('messidor_features.csv', index=False)
 
-# Print the number of null values before imputation
-print("Number of null values before imputation:")
-print(df.isnull().sum())
+# 2. Load dataset, visualize and drop uninformative columns
+df = pd.read_csv('messidor_features.csv')
+X = df.iloc[:,:-1].values.astype(np.float32)
+y = df.iloc[:,-1].values.astype(np.int32)
+plt.hist(X)
+plt.show()
+df.drop(columns=['Quality Assessment', 'OPTIC Disc', 'Output2(Can be Dropped)'], inplace=True)
 
-# Convert all columns to float type
-df = df.astype(float)
-
-# Impute missing values using KNN imputation
-imputer = KNNImputer(n_neighbors=5)
-df_imputed = pd.DataFrame(imputer.fit_transform(df))
-
-# This line converts the arff that was in the pandas Dataframe 'df' into a CSV file
-df_imputed.to_csv('file.csv', index=False, header=headerList)
-
-# Print a few rows of the imputed dataframe
-print("Imputed dataframe:")
-print(df_imputed.head())
-
-# Print the number of null values after imputation
-print("Number of null values after imputation:")
-print(df_imputed.isnull().sum())
-
-# This line sets a variable to be able to read the document & then print it
-pdf = pd.read_csv('file.csv')
-print(pdf)
-
-# END
-
-# Task 1 - part 2 & 3
-# Separate the input features and class labels
-X = pdf.drop(['Output', 'Output2(Can be Dropped)'], axis=1)
-y = pdf['Output2(Can be Dropped)']
-
-# Normalize the input features
-scaler = StandardScaler()
+# 3. Normalize the features
+scaler = MinMaxScaler()
 X_norm = scaler.fit_transform(X)
 
-# Split the dataset into training and testing sets
+# 4. Split dataset into training and testing sets
 X_train, X_test, y_train, y_test = train_test_split(X_norm, y, test_size=0.2, random_state=42)
 
+# 5. Build the neural network
+X_shape = X_train.shape[1]
+Y_shape = len(np.unique(y_train))
 
-# Visualize the distribution of features and classes
-# Histogram for visualization
-pdf.drop(['Output', 'Output2(Can be Dropped)'], axis=1).hist(bins=20, figsize=(20, 15))
-plt.show()
+model = tf.keras.Sequential([
+  tf.keras.layers.Dense(20, input_shape=(X_shape,), activation='relu'),
+  tf.keras.layers.Dense(10, activation='relu'),
+  tf.keras.layers.Dense(Y_shape, activation='softmax')
+])
 
-# Here is where I will drop the column(s) which I think do not contribute towards classification, for example
-X_train = np.delete(X_train, [3], axis=1)
-X_test = np.delete(X_test, [3], axis=1)
+model.compile(optimizer='adam', loss='sparse_categorical_crossentropy', metrics=['accuracy'])
 
-# END
+# Train the model
+history = model.fit(X_train, y_train, epochs=100, batch_size=32, validation_split=0.1)
+
+# Evaluate the model
+test_loss, test_acc = model.evaluate(X_test, y_test, verbose=2)
+print('\nTest accuracy:', test_acc)
 
